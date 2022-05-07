5b208d841d1a708fc4c380b9c62e690127b05930
RIYANSHGARG <riyanshgarg2103@gmail.com>
2022-05-07 13:09:36 +0530

    Added smart contract and metamask auth

---
+++ twitter-clone-blockchain/client/lib/costants.js
@@ -0,0 +1,9 @@
+// import ProfileImageMinterContractAbi from './ProfileImageNfts.json'
+import ProfileImageMinterContractAbi from './ProfileImageNfts.json'
+
+// import contractArtifact from './ProfileImageNfts.json'
+
+export const contractABI = ProfileImageMinterContractAbi.abi
+// export const contractABI = contractArtifact.abi
+export const contractAddress = "0x682Eb4f6a1f02360c7509A9eb2BD92aC4B6C1040"
+
---
+++ twitter-clone-blockchain/smart-contract/hardhat.config.js
@@ -0,0 +1,14 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.2",
+  networks: {
+    rinkeby: {
+      url:'https://eth-rinkeby.alchemyapi.io/v2/DBCttqbbmZv22C61y5JYZ5lBw_WdvD9A',
+      accounts: [
+        'd28301cc4263261fa88028203106265e94caa04af5646252391a684793568c47',
+      ],
+    },
+  },
+};
+ 
